47b141457671457899bbcced7f322450fee03e72
Anna Reis <reis.annabeatriz@outlook.com>
2021-08-26 16:56:01 -0300

    projeto final

--- js/index.js
+++ js/index.js
@@ -114,25 +114,25 @@ btnInsert.addEventListener("click", () => {
     // Checando a occorrencia da letra posicao ou nao encontrada e atribuindo a uma variavel
     let occurrences = game.findLetterOccurrence(letter)
 
-    // // condicao de vitoria e erro
-    checkGameResult()
-    
     // se a ocorrencia for um array vazio entre em attemps se nao adicione a letra
     if (occurrences.length === 0) {
         game.storeErrors(letter)
+        // condicao de vitoria e erro
+        checkGameResult()
         if (game.isLetter(letter) ) {
             attempts.innerHTML = Array.from(game.wrong.values()).join(" ")
             let errorImage = game.getErrorImage()
-            console.log("Handling error: ", errorImage)
-            if (errorImage !== undefined)
+            if (errorImage !== undefined) {
                 hangman.setAttribute("src", errorImage)
+            }
         }
         return
     } else {
         game.addLetters(occurrences, letter)
         words.innerHTML = game.capitalize(game.getHiddenWord()) 
+        // condicao de vitoria e erro
+        checkGameResult()
     }
-    // checkGameResult()
 
 })
 
